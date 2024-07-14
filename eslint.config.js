3892b553707324c05d096919f4276e4db78c1a12
jhossepmartinez <jhossepmv@gmail.com>
2024-07-14 14:04:41 -0400

    Updated eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals"
+import js from '@eslint/js';
+import globals from 'globals';
+import stylisticJs from '@stylistic/eslint-plugin-js';
 
 export default [
     js.configs.recommended,
 
     {
+        plugins: {
+            '@stylistic/js': stylisticJs
+        },
         rules: {
-            "no-unused-vars": "warn",
-            "no-undef": "warn"
+            'indent': ['error', 4],
+            'no-unused-vars': 'warn',
+            'no-undef': 'warn',
+            'semi': 'warn',
+            '@stylistic/js/quotes': ['error', 'single']
         },
         languageOptions: {
             globals: {
