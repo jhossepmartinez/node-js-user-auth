831be017ecb0e892252fb87952fd34483a5520fb
jhossepmartinez <jhossepmv@gmail.com>
2024-07-14 13:24:27 -0400

    Add initial routes

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const =;
+import express from "express"
+import {PORT} from "./config.js"
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello WOWOWO!")
+})
+
+app.post("/login", (req, res) => {})
+app.post("/register", (req, res) => {})
+app.post("/logout", (req, res) => {})
+
+app.post("/protected", (req, res) => {})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
