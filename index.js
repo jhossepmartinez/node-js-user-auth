f0895bc77b830db9b413b2d0f6112d19f4254b7f
jhossepmartinez <jhossepmv@gmail.com>
2024-07-14 16:40:15 -0400

    add basic user creation

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 import express from 'express';
-import {PORT} from './config.js';
+import { PORT } from './config.js';
+import { UserRepository } from './user-repository.js';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello WOWOWO!');
 });
 
 app.post('/login', (req, res) => {
-    res.json({ user: 'Jhossep'});
+    res.json({ user: 'Jhossep' });
 });
-app.post('/register', (req, res) => {});
-app.post('/logout', (req, res) => {});
 
-app.post('/protected', (req, res) => {});
+app.post('/register', (req, res) => {
+    const { username, password } = req.body;
+    console.log(req.body);
+    try {
+        const id = UserRepository.create({ username, password });
+        res.send({ id });
+    } catch (error) {
+        res.status(400).send(error.message);
+
+    }
+});
+
+app.post('/logout', (req, res) => { });
+
+app.post('/protected', (req, res) => { });
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
--- user-repository.js
+++ user-repository.js
@@ -1,5 +1,6 @@
 import DBLocal from 'db-local';
 const { Schema } = new DBLocal({ path: './db' });
+import crypto from 'crypto';
 
 const User = Schema('User', {
     _id: { type: String, required: true },
@@ -8,6 +9,27 @@ const User = Schema('User', {
 });
 
 export class UserRepository {
-    static create ({ username, password }) {}
+    static create ({ username, password }) {
+        // Validate username and password
+        if (typeof username !== 'string') throw new Error('username must be a string');
+        if (username.length < 3) throw new Error('username must have at least 3 characters');
+
+        if (typeof password !== 'string') throw new Error('password must be a string');
+        if (password.length < 6) throw new Error('password must have at least 6 characters');
+
+        const user = User.findOne({ username });
+        if (user) throw new Error('username already exists');
+        
+        const id = crypto.randomUUID();
+
+        User.create({
+            _id: id,
+            username,
+            password
+        }).save();
+
+        return id;
+    }
+
     static login ({ username, password }) {}
 }
