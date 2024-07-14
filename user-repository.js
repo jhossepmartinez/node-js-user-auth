51857798b18d473e83712a5fc008a06c88320f10
jhossepmartinez <jhossepmv@gmail.com>
2024-07-14 14:09:25 -0400

    Add initial database schema

--- user-repository.js
+++ user-repository.js
@@ -1,1 +1,13 @@
 import DBLocal from 'db-local';
+const { Schema } = new DBLocal({ path: './db' });
+
+const User = Schema('User', {
+    _id: { type: String, required: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+export class UserRepository {
+    static create ({ username, password }) {}
+    static login ({ username, password }) {}
+}
