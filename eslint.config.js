de7c02de67db1863254c31ba8622059ee66b90fb
jhossepmartinez <jhossepmv@gmail.com>
2024-07-14 12:06:21 -0400

    Add config files

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals"
+
+export default [
+    js.configs.recommended,
+
+    {
+        rules: {
+            "no-unused-vars": "warn",
+            "no-undef": "warn"
+        },
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        }
+    }
+];
